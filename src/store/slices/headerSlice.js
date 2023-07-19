56f4a90ba7381d99efc4bbfb4703e83cc932c4b7
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2023-07-19 17:18:03 +0300

    Refactored project

--- src/store/slices/headerSlice.js
+++ src/store/slices/headerSlice.js
@@ -2,43 +2,47 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { API_BASE_URL } from 'constants/api';
 
 const fetchHeaderData = createAsyncThunk(
-  'header/fetchData', 
+  'header/getData',
   async (_, thunkApi) => {
-    
     const url = `${API_BASE_URL}/header/.json`;
-    
+    console.log(url)
+    console.log(API_BASE_URL)
+
     try {
       const response = await fetch(url);
-      const headerData = await response.json();
-      if (!headerData) throw new Error('Failed to fetch');
-      return thunkApi.fulfillWithValue(headerData);
+      const data = await response.json();
+      if (!data) throw new Error('Failed to fetch');
+      return thunkApi.fulfillWithValue(data);
     }
     catch (error) {
       console.error(error);
-      return thunkApi.rejectWithValue(error.message);
+      const { message } = error;
+      return thunkApi.rejectWithValue(message);
     }
   }
 );
 
 const initialState = {
   isLoading: false,
-  headereData: null,
-  errorMessage: null,
+  headerData: null,
+  errorMessage: '',
 };
 
 const headerSlice = createSlice({
   name: 'header',
   initialState,
   extraReducers: {
-    [fetchHeaderData.pending]: (state) => {
+    [`${fetchHeaderData.pending}`]: (state) => {
       state.isLoading = true;
+      state.headerData = null;
+      state.errorMessage = '';
     },
-    [fetchHeaderData.fulfilled]: (state, { payload }) => {
+    [`${fetchHeaderData.fulfilled}`]: (state, { payload }) => {
       state.isLoading = false;
       state.headerData = payload;
-      state.errorMessage = null;
+      state.errorMessage = '';
     },
-    [fetchHeaderData.rejected]: (state, { payload }) => {
+    [`${fetchHeaderData.rejected}`]: (state, { payload }) => {
       state.isLoading = false;
       state.headerData = null;
       state.errorMessage = payload;
@@ -47,4 +51,4 @@ const headerSlice = createSlice({
 });
 
 export { fetchHeaderData };
-export const { reducer: headerReducer} = headerSlice;
+export const { reducer: headerReducer } = headerSlice;
