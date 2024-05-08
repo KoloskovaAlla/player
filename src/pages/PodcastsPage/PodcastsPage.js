97cd8b4d30619cabbcd25e3ae2e101256629160f
allakoloskova <66311342+allakoloskova@users.noreply.github.com>
2024-05-08 17:45:50 +0300

    Added preloader

--- src/pages/PodcastsPage/PodcastsPage.js
+++ src/pages/PodcastsPage/PodcastsPage.js
@@ -2,7 +2,7 @@ import classes from './PodcastsPage.module.scss';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { usePodcasts, useModal } from 'hooks';
-import { PodcastPreview, ModalPlayer } from 'components/layout';
+import { PodcastPreview, ModalPlayer, Preloader } from 'components/layout';
 
 export const PodcastsPage = () => {
   const dispatch = useDispatch();
@@ -13,11 +13,7 @@ export const PodcastsPage = () => {
     dispatch(fetchPodcastsData());
   }, [dispatch, fetchPodcastsData]);
 
-  if (!podcastsData) return (
-    <div className={classes.wrapper}>
-      <div className={classes.nopodcasts}>Скоро здесь появятся треки</div>
-    </div>
-  );
+  if (!podcastsData) return <Preloader />
 
   if (podcastsData) return (
     <div className={classes.wrapper}>
